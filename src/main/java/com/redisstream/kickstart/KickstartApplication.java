4189dc7fa7a0c088579f8417b08ab97968f92e54
amitiamity <amitptl.in@gmail.com>
2020-05-15 21:38:50 +0400

    project setup

---
+++ src/main/java/com/redisstream/kickstart/KickstartApplication.java
@@ -0,0 +1,13 @@
+package com.redisstream.kickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redisstream/kickstart/KickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redisstream.kickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
